3fa9e5501cadc351e7ff5444024c1e1fa1e8a4bd
Tahel Karavani <tahelka@mta.ac.il>
2022-05-14 11:27:19 +0300

    hellooooo

--- project24.4/project24.4/concert.h
+++ project24.4/project24.4/concert.h
@@ -1,17 +1,16 @@
 #ifndef __CONCERT_H
 #define __CONCERT_H
-
+// ori and ta
 #define _CRT_SECURE_NO_WARNINGS
 
 #define TOTAL_SECONDS 60.00
-
+/// taaaaaaaaaaaaaaaaaaa nna
 #include <stdio.h>
 #include <stdlib.h>
 #include "trees.h"
 #include "utils.h"
 #include "concertLists.h"
 #include "lists.h"
-
 typedef struct date
 {
 	int day, month, year;
